b9d4ed6d5ed2c8e49a70a0641015c18cda885500
Olexanddr <48255226+Olexanddr@users.noreply.github.com>
2022-07-28 22:53:13 +0300

    Update db.php

--- conf/db.php
+++ conf/db.php
@@ -1,12 +1,11 @@
 <?php
 	class DB{
 		const USER = "b962b824a8327d";
-		const PASSWORD = "9bd62f51";
+		const PASSWORD = "657cc1ef46e58ae";
 		const HOST = "eu-cdbr-west-03.cleardb.net";
 		const DB = "heroku_dc0e8d8db09d6e3";
 
 		public static function connToDB(){
-			echo 1;
 			$user = self::USER;
 			$pass = self::PASSWORD;
 			$host = self::HOST;
