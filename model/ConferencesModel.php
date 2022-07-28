7a8e2049f92f8a3bdc043c7fecd017d80d03ae36
Olexanddr <48255226+Olexanddr@users.noreply.github.com>
2022-07-28 21:16:18 +0300

    Update ConferencesModel.php

--- model/ConferencesModel.php
+++ model/ConferencesModel.php
@@ -1,7 +1,7 @@
 <?php
 	class ConferencesModel extends Model{
 		public function getTitles(){
-			$query = $this->db->query("SELECT id,title,data FROM test.conferences");
+			$query = $this->db->query("SELECT id,title,data FROM heroku_dc0e8d8db09d6e3.conferences");
 			$row = $query->fetchAll(PDO::FETCH_ASSOC);
 			for($i = 0;$i < count($row);$i++){
 				$row[$i]["data"] = str_replace('T',' ',$row[$i]["data"]);
