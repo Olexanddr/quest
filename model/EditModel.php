5da2d8bb99edd29357d1c6b085439947ef7da70e
Olexanddr <48255226+Olexanddr@users.noreply.github.com>
2022-07-28 21:17:27 +0300

    Update EditModel.php

--- model/EditModel.php
+++ model/EditModel.php
@@ -2,13 +2,13 @@
 	class EditModel extends Model{
 		public function getInfo($id){
 			$data = array();
-			$query = $this->db->prepare("SELECT * FROM test.conferences WHERE id = :id");
+			$query = $this->db->prepare("SELECT * FROM heroku_dc0e8d8db09d6e3.conferences WHERE id = :id");
 			$query->execute(['id' => $id]);
 			$result = $query->fetchAll(PDO::FETCH_ASSOC);
 			return $result[0];
 		}
 		public function setInfo($info){
-			$query = $this->db->prepare("UPDATE test.conferences SET title = :title, data = :data, latitude = :latitude, longitude = :longitude, country = :country WHERE id = :id");
+			$query = $this->db->prepare("UPDATE heroku_dc0e8d8db09d6e3.conferences SET title = :title, data = :data, latitude = :latitude, longitude = :longitude, country = :country WHERE id = :id");
 			$query->execute($info);
 			$result = $query->fetchAll(PDO::FETCH_ASSOC);
 			header("Location: /conferences");
