eae416d9001caec47c3c1666c72bbf2d67a3216c
Olexanddr <48255226+Olexanddr@users.noreply.github.com>
2022-07-28 21:16:41 +0300

    Update CreateModel.php

--- model/CreateModel.php
+++ model/CreateModel.php
@@ -1,7 +1,7 @@
 <?php
 	class CreateModel extends Model{
 		public function saveNew($arr){
-			$query = $this->db->prepare("INSERT INTO test.conferences(title, data, latitude, longitude, country) VALUES (:title, :data, :latitude, :longitude, :country);");
+			$query = $this->db->prepare("INSERT INTO heroku_dc0e8d8db09d6e3.conferences(title, data, latitude, longitude, country) VALUES (:title, :data, :latitude, :longitude, :country);");
 			$query->execute($arr);
 			$result = $query->fetchAll(PDO::FETCH_ASSOC);
 		}
